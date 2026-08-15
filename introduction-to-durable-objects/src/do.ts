61f90e9c83cc7374ae052f1131bfc35c17108bbb
Yongmin0423 <yym21345@gmail.com>
2026-08-16 01:21:18 +0900

    Using Durable Objects

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -1,3 +1,8 @@
 import { DurableObject } from "cloudflare:workers";
 
-export class DurablePotato extends DurableObject<Env> {}
+export class DurablePotato extends DurableObject<Env> {
+    // cloudflare의 청사진 같은 역할
+	ping() {
+		return 'pong';
+	}
+}
--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -2,6 +2,10 @@ export {DurablePotato} from "./do";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		// DurablePotato의 stub를 불러온다. 
+		// 해당 stub가 존재하지 않으면 새로 생성한다.
+		// 해당 default라는 이름의 durable object는 유일하게 존재하게 된다.
+		const stub = env.DP.getByName('default');
+		return new Response(await stub.ping());
 	},
 } satisfies ExportedHandler<Env>;

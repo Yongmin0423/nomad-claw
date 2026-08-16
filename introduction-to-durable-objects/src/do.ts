a288b567abc89060f298519bf20521b50657fde0
Youngmin <Youngmin.You@loword.co.kr>
2026-08-16 16:35:33 +0900

    Durable object lifecycle

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -2,7 +2,12 @@ import { DurableObject } from "cloudflare:workers";
 
 export class DurablePotato extends DurableObject<Env> {
     // cloudflare의 청사진 같은 역할
-	ping() {
-		return 'pong';
+	// 아무 동작 하지 않으면 idle상태로 변하는데, 그 이후에 일정 시간 아무 동작이 없으면 hibernate 된다.
+	// hibernate된 상태에서도 cold start 없이 바로 깨어난다.
+	count = 0;
+	increase() {
+		this.count++;
+		return `count is ${this.count}`
 	}
+
 }
--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -2,10 +2,14 @@ export {DurablePotato} from "./do";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		// DurablePotato의 stub를 불러온다. 
-		// 해당 stub가 존재하지 않으면 새로 생성한다.
-		// 해당 default라는 이름의 durable object는 유일하게 존재하게 된다.
-		const stub = env.DP.getByName('default');
-		return new Response(await stub.ping());
+		const { pathname } = new URL(request.url);
+		if (pathname === "/") {
+			const dp = env.DP.getByName('default');
+			//네트워크 작업이 필요하기 때문에 await 키워드를 붙인다.
+			return new Response(await dp.increase());
+		}
+		return new Response(null, {
+			status: 404,
+		});
 	},
 } satisfies ExportedHandler<Env>;

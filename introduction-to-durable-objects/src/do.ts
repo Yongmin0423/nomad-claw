575f4cbb8fc9c74e9be47e180422282928f3f479
Youngmin <Youngmin.You@loword.co.kr>
2026-08-19 08:55:31 +0900

    [yuyongmin][ Durable Object SQLite 초기화 및 EU jurisdiction 라우팅 추가 ]

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -4,6 +4,22 @@ export class DurablePotato extends DurableObject<Env> {
     // cloudflare의 청사진 같은 역할
 	// 아무 동작 하지 않으면 idle상태로 변하는데, 그 이후에 일정 시간 아무 동작이 없으면 hibernate 된다.
 	// hibernate된 상태에서도 cold start 없이 바로 깨어난다.
+
+	constructor(ctx:DurableObjectState,env: Env) {
+		super(ctx, env);
+		console.log('do started!!');
+		const res = ctx.storage.sql.exec(`
+			CREATE TABLE IF NOT EXISTS pongs (
+			 id INTEGER PRIMARY KEY AUTOINCREMENT,
+			 total INTEGER
+			)
+			`);
+
+			ctx.storage.sql.exec(`
+				INSERT OR IGNORE INTO pongs (id, total) VALUE (1, 0);
+				`)
+		console.log("res", res);
+	}
 	count = 0;
 	increase() {
 		this.count++;
--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -4,7 +4,7 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const { pathname } = new URL(request.url);
 		if (pathname === "/") {
-			const dp = env.DP.getByName('default');
+			const dp = env.DP.jurisdiction("eu").getByName('default', {locationHint: 'eeur'});
 			//네트워크 작업이 필요하기 때문에 await 키워드를 붙인다.
 			return new Response(await dp.increase());
 		}

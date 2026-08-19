d4aa579325c7fc922eace41d401aa3fd9446f588
Yongmin0423 <yym21345@gmail.com>
2026-08-19 22:40:29 +0900

    concurrency & Alarms

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -4,11 +4,13 @@ export class DurablePotato extends DurableObject<Env> {
     // cloudflare의 청사진 같은 역할
 	// 아무 동작 하지 않으면 idle상태로 변하는데, 그 이후에 일정 시간 아무 동작이 없으면 hibernate 된다.
 	// hibernate된 상태에서도 cold start 없이 바로 깨어난다.
-
+	sql: SqlStorage;
 	constructor(ctx:DurableObjectState,env: Env) {
 		super(ctx, env);
-		console.log('do started!!');
-		const res = ctx.storage.sql.exec(`
+
+		this.sql = ctx.storage.sql;
+
+		const res = this.sql.exec(`
 			CREATE TABLE IF NOT EXISTS pongs (
 			 id INTEGER PRIMARY KEY AUTOINCREMENT,
 			 total INTEGER
@@ -16,14 +18,27 @@ export class DurablePotato extends DurableObject<Env> {
 			`);
 
 			ctx.storage.sql.exec(`
-				INSERT OR IGNORE INTO pongs (id, total) VALUE (1, 0);
+				INSERT OR IGNORE INTO pongs (id, total) VALUES (1, 0);
 				`)
-		console.log("res", res);
-	}
-	count = 0;
-	increase() {
-		this.count++;
-		return `count is ${this.count}`
-	}
+	};
 
-}
+	async increase() {
+		const {total} = this.sql.exec('UPDATE pongs SET total = total + 1 WHERE id = 1 RETURNING total').one() as {total: number};
+		
+		if(total >= 30) {
+			const currentAlarm = await this.ctx.storage.getAlarm();
+
+			if(currentAlarm === null) {
+				 this.ctx.storage.setAlarm(Date.now() + 10 * 1000);
+			}
+		}
+		return `count is ${total}`;
+	}
+	
+	alarm() {
+		this.sql.exec('UPDATE pongs SET total = 0 WHERE id = 1');
+		// search in your alarms table and find the next alarm
+		// schedule the next alarm	
+		// 이렇게 하는 이유는 durable object는 각 하나의 알람밖에 가질 수 없다.
+	}
+}  
--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -2,9 +2,11 @@ export {DurablePotato} from "./do";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const { pathname } = new URL(request.url);
+		const { pathname, searchParams} = new URL(request.url);
+			const nickname = searchParams.get('nickname') ?? 'anon';
+
 		if (pathname === "/") {
-			const dp = env.DP.jurisdiction("eu").getByName('default', {locationHint: 'eeur'});
+			const dp = env.DP.getByName('default');
 			//네트워크 작업이 필요하기 때문에 await 키워드를 붙인다.
 			return new Response(await dp.increase());
 		}

81a112599b8cadaeb80626db3cb9a3412d59f002
Yongmin0423 <yym21345@gmail.com>
2026-08-29 22:26:41 +0900

    sql storage and authentication

--- agent-foundations/src/App.tsx
+++ agent-foundations/src/App.tsx
@@ -2,30 +2,61 @@ import { useAgent } from "agents/react"
 import type { ChattingRoomAgent, ChattingRoomState } from "../worker";
 import { useState } from "react";
 
+type Message = {
+	id: number, nickname: string, message:string, created_at: number
+}
+
 function App() {
 	const [isConnected, setIsConnected] = useState(false);
 	const [message, setMessage] = useState(''); 
+	const [messages, setMessages] = useState<Message[]>([]);
+	const [nickname, setNickname] = useState<string | null>(null);
+	const [ready, setReady] = useState(false);
+	
 	//이걸로 서버랑 연결되서 웹소켓 연결이 되었는지, 끊어졌는지, 에러가 났는지 알 수 있다.'
 	//useAgent는 Generics를 사용해서 어떤 에이전트를 사용할건지, 어떤 state를 사용할건지 알려준다.
 	const agent  = useAgent<ChattingRoomAgent, ChattingRoomState>({
 		agent: 'ChattingRoomAgent',
-		onOpen: () => setIsConnected(true),
+		query: {
+			name: nickname
+		},
+		enabled: ready,
+		onOpen: async() => {
+			setIsConnected(true)
+			const history = await agent?.stub.loadHistory() as Message[];
+			setMessages(history);
+		},
 		// onStateUpdate: (state) => setPingPongs(state.pingPongCount),
 		onClose: () => setIsConnected(false),
 		onError: () => setIsConnected(false),
-		onMessage: (event) => console.log(event),
+		onMessage: (event) => setMessages(prev => [...prev, JSON.parse(event.data)] as Message[]),
 	})
 
 	const sendMessage = () => {
+		agent.send(message)
 		setMessage('');	
 	}
 
-	if(!isConnected) return <h1>connecting....</h1>
+	const onConfirm = () => {
+		setReady(true);
+	}
+
+	if(!isConnected) {return <div>
+				<h1>who are you?</h1>
+				<input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} placeholder="Type a nickname" autoFocus />
+				<button onClick={onConfirm}>confirm</button>
+			</div>}
 	
 	return <div>
 		<h1>Chatting Room Agent</h1>
 		<h3>Online ppl: {agent?.state?.currentlyOnline}</h3>
 		<hr/>
+		{messages.map((message) => (
+			<div key={message.id}>
+				<span>{message.nickname}: </span>
+				<span>{message.message}</span>
+			</div>
+		))}
 		<form onSubmit={(e) => {
 			e.preventDefault();
 			sendMessage();
--- agent-foundations/worker/index.ts
+++ agent-foundations/worker/index.ts
@@ -1,4 +1,4 @@
-import { Agent, callable, routeAgentRequest, type Connection, type WSMessage } from 'agents';
+import { Agent, callable, getCurrentAgent, routeAgentRequest, type Connection, type WSMessage } from 'agents';
 
 export type ChattingRoomState = {
 	currentlyOnline: number;
@@ -23,7 +23,14 @@ export class ChattingRoomAgent extends Agent<Env,ChattingRoomState> {
 		});
 	}
 
-	onConnect(): void | Promise<void> {
+	onConnect(connection:Connection, ctx: ConnectionContext): void | Promise<void> {
+		const url = new URL(ctx.request.url);
+		const nickname = url.searchParams.get('name') ?? 'anon';
+
+		connection.setState({
+			nickname,
+		})
+
 		this.setState({
 			currentlyOnline: this.state.currentlyOnline + 1
 		})
@@ -35,15 +42,49 @@ export class ChattingRoomAgent extends Agent<Env,ChattingRoomState> {
 		})	
 	}
 
-	onMessage(connection: Connection, message: WSMessage) {
-		console.log(message);
-		connection.send(message)
+	onMessage(connection: Connection<{nickname:string}>, message: WSMessage) {
+		const messageObj = {
+			nickcname: connection.state.nickname,
+			message: message.toString(),
+			created_at: Date.now(),
+		};
+		void this.sql`
+		INSERT INTO messages (nickname, message, created_at) VALUES(${messageObj.nickcname}, ${messageObj.message}, ${messageObj.created_at});
+		`
+		//this.broadcast(JSON.stringify(messageObj), [connection.id]);
+		this.broadcast(JSON.stringify(messageObj));
+
 	}
+
+	@callable()
+	loadHistory(){
+		const {connection} = getCurrentAgent<ChattingRoomAgent>();
+		console.log(connection.state);
+		return this.sql`SELECT * FROM messages ORDER BY created_at ASC LIMIT 100`
+	}
+
+	onStart(){
+		void this.sql`
+		CREATE TABLE IF NOT EXISTS messages (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			nickname TEXT NOT NULL,
+			message TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+		)
+		`
+	}
+		
+	
 
 	// onStateChanged(state: ChattingRoomState, source: Connection | 'server'): void {
 	// 	console.log("new state", state);
 	// 	console.log('who did it', source)
 	// }
+
+	validateStateChange(_nextState: ChattingRoomState, source: Connection | 'server'): void {
+		if (source !== 'server') throw new Error('cant do this');
+
+	}
 }
 
 export default {
